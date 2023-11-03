67e62adfd0c738bd008d651101a35ccdc2162ec8
Remi Collin <sharon.code@proton.me>
2023-11-03 16:47:21 +0100

    add component performance

--- src/components/Performance/index.jsx
+++ src/components/Performance/index.jsx
@@ -1,5 +1,6 @@
 import './performance.css';
 import { useFetch } from '../../utils/hooks';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis } from 'recharts';
 
 function Performance({ userId }) {
    const { isLoading, data, error } = useFetch(
@@ -10,7 +11,29 @@ function Performance({ userId }) {
    }
    if (!isLoading) {
       const performance = data.data;
-      return <div className="performance-container">Performance</div>;
+      return (
+         <div className="performance-container">
+            <RadarChart
+               cx={150}
+               cy={150}
+               outerRadius={120}
+               width={300}
+               height={300}
+               data={performance.data}
+            >
+               <PolarGrid />
+               <PolarAngleAxis dataKey="kind" />
+               <Radar
+                  name="Mike"
+                  dataKey="value"
+                  stroke="var(--primary)"
+                  fill="var(--primary)"
+                  fillOpacity={0.7}
+               />
+            </RadarChart>
+         </div>
+      );
+      /* return <div className="performance-container">Performance</div>; */
    }
 }
 
