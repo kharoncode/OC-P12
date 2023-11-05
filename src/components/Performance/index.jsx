efdb86d49b96a111c4ccb1571d1c0bfc07873649
Remi Collin <sharon.code@proton.me>
2023-11-05 21:02:24 +0100

    custom component performance color

--- src/components/Performance/index.jsx
+++ src/components/Performance/index.jsx
@@ -1,6 +1,12 @@
 import './performance.css';
 import { useFetch } from '../../utils/hooks';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis } from 'recharts';
+import {
+   Radar,
+   RadarChart,
+   PolarGrid,
+   PolarAngleAxis,
+   ResponsiveContainer,
+} from 'recharts';
 
 function Performance({ userId }) {
    const { isLoading, data, error } = useFetch(
@@ -11,30 +17,50 @@ function Performance({ userId }) {
    }
    if (!isLoading) {
       const kind = data.data.kind;
+      const test = {
+         1: 'Cardio',
+         2: 'Energie',
+         3: 'Endurance',
+         4: 'Force',
+         5: 'Vitesse',
+         6: 'Intensité',
+      };
+      console.log(kind);
       const performance = data.data.data.map((el) => {
-         return { value: el.value, kind: kind[el.kind] };
+         return { value: el.value, kind: test[el.kind] };
       });
 
       return (
          <div className="performance-container center">
-            <RadarChart
-               cx="50%"
-               cy="50%"
-               outerRadius="70%"
-               width={260}
-               height={260}
-               data={performance}
-            >
-               <PolarGrid radialLines={false} />
-               <PolarAngleAxis dataKey="kind" />
-               <Radar
-                  name="Mike"
-                  dataKey="value"
-                  stroke="var(--primary)"
-                  fill="var(--primary)"
-                  fillOpacity={0.7}
-               />
-            </RadarChart>
+            <ResponsiveContainer width={290} height={240}>
+               <RadarChart
+                  cx="50%"
+                  cy="50%"
+                  // outerRadius="65%"
+                  // width={258}
+                  // height={263}
+                  data={performance}
+                  startAngle={30}
+                  endAngle={-330}
+               >
+                  <PolarGrid radialLines={false} />
+                  <PolarAngleAxis
+                     dataKey="kind"
+                     tick={{
+                        fill: 'white',
+                        fontSize: '0.75em',
+                        fontWeight: 500,
+                     }}
+                  />
+                  <Radar
+                     name="Performance"
+                     dataKey="value"
+                     stroke="var(--primary)"
+                     fill="var(--primary)"
+                     fillOpacity={0.7}
+                  />
+               </RadarChart>
+            </ResponsiveContainer>
          </div>
       );
       /* return <div className="performance-container">Performance</div>; */
