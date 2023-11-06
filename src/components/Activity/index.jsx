5af0881a4a3f54da000613638135f6277770f767
Remi Collin <sharon.code@proton.me>
2023-11-06 16:07:52 +0100

    init activity component

--- src/components/Activity/index.jsx
+++ src/components/Activity/index.jsx
@@ -1,4 +1,14 @@
 import './activity.css';
+import {
+   BarChart,
+   Bar,
+   XAxis,
+   YAxis,
+   CartesianGrid,
+   Tooltip,
+   ResponsiveContainer,
+   Legend,
+} from 'recharts';
 import { useFetch } from '../../utils/hooks';
 
 function Activity({ userId }) {
@@ -10,7 +20,46 @@ function Activity({ userId }) {
    }
    if (!isLoading) {
       const activity = data.data.sessions;
-      return <div className="activity-container">Activity {userId}</div>;
+      // Récupérer le poids le plus petit et le poids le plus grand.
+      // y ajouter +/1 1kg pour le repère vertical
+      return (
+         <div className="activity-container center">
+            <ResponsiveContainer width={835} height={320}>
+               <BarChart
+                  // margin={{ top: 25, right: 25, bottom: 25, left: 25 }}
+                  width={500}
+                  height={300}
+                  data={activity}
+                  barGap={8}
+                  barCategoryGap={10}
+                  barSize={7}
+               >
+                  <CartesianGrid vertical={false} strokeDasharray="3 3" />
+                  <XAxis dataKey="da" tickLine={false} />
+                  <YAxis
+                     orientation="right"
+                     axisLine={false}
+                     tickLine={false}
+                     dataKey="kilogram"
+                     type="number"
+                     domain={[68, 71]}
+                  />
+                  <Tooltip />
+                  <Legend verticalAlign="top" align="right" />
+                  <Bar
+                     dataKey="kilogram"
+                     fill="#282D30"
+                     radius={[5, 5, 0, 0]}
+                  />
+                  <Bar
+                     dataKey="calories"
+                     fill="var(--primary)"
+                     radius={[5, 5, 0, 0]}
+                  />
+               </BarChart>
+            </ResponsiveContainer>
+         </div>
+      );
    }
 }
 
